850f81a100157a7b927a758997d5fa8143b9506c
Jeffery <up937100@myport.ac.uk>
2020-10-06 19:37:02 +0100

    Added dirty hands and click multiplier

--- Coronamental/main.js
+++ Coronamental/main.js
@@ -1,5 +1,7 @@
 var cells = 0;
-var fly_building = 0;
+var upgrade_click = 1;
+var building_flies = 0;
+var building_hands = 0;
 
 
 function infectClick(number){
@@ -7,19 +9,49 @@ function infectClick(number){
 	document.getElementById('cells_amount').innerHTML = cells;
 };
 
-function buyBuildingFly(){
-	var fly_building_cost = Math.floor(10 * Math.pow(1.1,fly_building));   //cost of this fly building
-	if (cells >= fly_building_cost){
-		fly_building = fly_building + 1;
-		cells = cells - fly_building_cost;
-		document.getElementById('building_fly_amount').innerHTML = fly_building;
+function buyUpgradeClick(){
+	var upgrade_click_cost = Math.floor(50 * Math.pow(1.75,upgrade_click-1));
+	if (cells >= upgrade_click_cost) {
+		upgrade_click = upgrade_click + 1;
+		cells = cells - upgrade_click_cost;
+		document.getElementById('upgrade_click_amount').innerHTML = upgrade_click;
 		document.getElementById('cells_amount').innerHTML = cells;
 	};
-	var nextCost = Math.floor(10 * Math.pow(1.1,fly_building));
-	document.getElementById('building_fly_cost').innerHTML = nextCost;
+	var nextCost = Math.floor(50 * Math.pow(1.75,upgrade_click-1));
+	document.getElementById('upgrade_click_cost').innerHTML = nextCost;
+	//document.getElementById('infectButton').setAttribute('onclick','infectClick(upgrade_click)');
+
 };
+
+function buyBuildingFlies(){
+	var building_flies_cost = Math.floor(10 * Math.pow(1.1,building_flies));   //cost of this fly building
+	if (cells >= building_flies_cost){
+		building_flies = building_flies + 1;
+		cells = cells - building_flies_cost;
+		document.getElementById('building_flies_amount').innerHTML = building_flies;
+		document.getElementById('cells_amount').innerHTML = cells;
+	};
+	var nextCost = Math.floor(10 * Math.pow(1.1,building_flies));
+	document.getElementById('building_flies_cost').innerHTML = nextCost;
+};
+
+function buyBuildingHands(){
+	var building_hands_cost = Math.floor(150 * Math.pow(1.15,building_hands));
+	if (cells >= building_hands_cost){
+		building_hands = building_hands + 1;
+		cells = cells - building_hands_cost;
+		document.getElementById('building_hands_amount').innerHTML = building_hands;
+		document.getElementById('cells_amount').innerHTML = cells;
+	};
+	
+	var nextCost = Math.floor(150 * Math.pow(1.15,building_hands));
+	document.getElementById('building_hands_cost').innerHTML = nextCost;
+}
 
 
 window.setInterval(function(){
-	infectClick(fly_building);
+	infectClick(building_flies);
+	infectClick(building_hands*5);
+	
 }, 1000);
+
