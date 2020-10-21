c7f10e143acb06a0aafb9571eff00042c38e2fa2
Jeffery <up937100@myport.ac.uk>
2020-10-21 19:56:33 +0100

    Fixes and small variable updates
    Fixed costs displayed incorrectly
    Updated costs to become a variable so it becomes easier to updates/change

--- Coronamental/main.js
+++ Coronamental/main.js
@@ -1,6 +1,7 @@
 var cells = 0;
 var building_multiplier = [1,10,50];
-var upgrade_cost = [50,250,1000,2500]
+var building_cost = [10,500,2500,10000];
+var upgrade_cost = [50,250,1000,2500];
 var upgrade_click = 1;
 var upgrade_flies = 1;
 var upgrade_hands = 1;
@@ -14,13 +15,13 @@ var cells_to_gamble = 0;
 function infectClick(number){
 	cells = cells + number;
 	document.getElementById('cells_amount').innerHTML = cells;
-};
+}
 
 function cellsPerSecond(){
 	var cps = 0;
 	cps = cps + (building_flies*building_multiplier[0]*upgrade_flies) + (building_hands*building_multiplier[1]*upgrade_hands) + (building_mask*building_multiplier[2]*upgrade_mask);
 	document.getElementById('cps').innerHTML = cps;
-};
+}
 
 function cpsUpdate(){
 	var building_flies_cps = 0;
@@ -41,40 +42,40 @@ function cpsUpdate(){
 
 //Buildings
 function buyBuildingFlies(){
-	var building_flies_cost = Math.floor(10 * Math.pow(1.1,building_flies));   //cost of this fly building
-	if (cells >= building_flies_cost){																				//checks if they have enough cells to buy
-		building_flies = building_flies + 1;																		//adds 1 to building
-		cells = cells - building_flies_cost;
-		document.getElementById('building_flies_amount').innerHTML = building_flies;  //updates the amount of building html
-		document.getElementById('cells_amount').innerHTML = cells;										//updates the cell amount on html
-	};
-	var nextCost = Math.floor(10 * Math.pow(1.1,building_flies));
-	document.getElementById('building_flies_cost').innerHTML = nextCost;
-};
+	var building_flies_cost = Math.floor(building_cost[0] * Math.pow(1.1,building_flies));   					//cost of this fly building
+	if (cells >= building_flies_cost){															//checks if they have enough cells to buy
+		building_flies = building_flies + 1;													//adds 1 to building
+		cells = cells - building_flies_cost;													//Takes cost away from cells
+		document.getElementById('building_flies_amount').innerHTML = building_flies;  			//updates the amount of building HTML
+		document.getElementById('cells_amount').innerHTML = cells;								//updates the cell amount on HTML
+	}
+	var nextCost = Math.floor(building_cost[0] * Math.pow(1.1,building_flies));								//Updates the cost for next building
+	document.getElementById('building_flies_cost').innerHTML = nextCost;						//Updates the cost on HTML
+}
 
 function buyBuildingHands(){
-	var building_hands_cost = Math.floor(150 * Math.pow(1.15,building_hands));
+	var building_hands_cost = Math.floor(building_cost[1] * Math.pow(1.15,building_hands));
 	if (cells >= building_hands_cost){
 		building_hands = building_hands + 1;
 		cells = cells - building_hands_cost;
 		document.getElementById('building_hands_amount').innerHTML = building_hands;
 		document.getElementById('cells_amount').innerHTML = cells;
-	};
-	var nextCost = Math.floor(150 * Math.pow(1.15,building_hands));
+	}
+	var nextCost = Math.floor(building_cost[1] * Math.pow(1.15,building_hands));
 	document.getElementById('building_hands_cost').innerHTML = nextCost;
-};
+}
 
 function buyBuildingMask(){
-	var building_mask_cost = Math.floor(1500 * Math.pow(1.2,building_mask));
+	var building_mask_cost = Math.floor(building_cost[2] * Math.pow(1.2,building_mask));
 	if (cells >= building_mask_cost){
 		building_mask = building_mask + 1;
 		cells = cells - building_mask_cost;
 		document.getElementById('building_mask_amount').innerHTML = building_mask;
 		document.getElementById('cells_amount').innerHTML = cells;
-	};
-	var nextCost = Math.floor(1500 * Math.pow(1.2,building_mask));
+	}
+	var nextCost = Math.floor(building_cost[2] * Math.pow(1.2,building_mask));
 	document.getElementById('building_mask_cost').innerHTML = nextCost;
-};
+}
 
 
 //Upgrades
@@ -85,11 +86,11 @@ function buyUpgradeClick(){
 		cells = cells - upgrade_click_cost;
 		document.getElementById('upgrade_click_amount').innerHTML = upgrade_click;
 		document.getElementById('cells_amount').innerHTML = cells;
-	};
+	}
 	var nextCost = Math.floor(upgrade_cost[0] * Math.pow(1.2,upgrade_click-1));
 	document.getElementById('upgrade_click_cost').innerHTML = nextCost;
 	//document.getElementById('infectButton').setAttribute('onclick','infectClick(upgrade_click)');
-};
+}
 
 function buyUpgradeFlies(){
 	var upgrade_flies_cost = Math.floor(upgrade_cost[1] * Math.pow(1.3,upgrade_flies-1));
@@ -98,11 +99,11 @@ function buyUpgradeFlies(){
 		cells = cells - upgrade_flies_cost;
 		document.getElementById('upgrade_flies_amount').innerHTML = upgrade_flies;
 		document.getElementById('cells_amount').innerHTML = cells;
-	};
+	}
 
 	var nextCost = Math.floor(upgrade_cost[1] * Math.pow(1.3,upgrade_flies-1));
 	document.getElementById('upgrade_flies_cost').innerHTML = nextCost;
-};
+}
 
 function buyUpgradeHands(){
 	var upgrade_hands_cost = Math.floor(upgrade_cost[2] * Math.pow(1.3,upgrade_hands-1));
@@ -111,42 +112,42 @@ function buyUpgradeHands(){
 		cells = cells - upgrade_hands_cost;
 		document.getElementById('upgrade_hands_amount').innerHTML = upgrade_hands;
 		document.getElementById('cells_amount').innerHTML = cells;
-	};
+	}
 	var nextCost = Math.floor(upgrade_cost[2] * Math.pow(1.3,upgrade_hands-1));
 	document.getElementById('upgrade_hands_cost').innerHTML = nextCost;
-};
+}
 
 function buyUpgradeMask(){
-	var upgrade_mask_cost = Math.floor(5000 * Math.pow(1.3,upgrade_mask-1));
+	var upgrade_mask_cost = Math.floor(upgrade_cost[3] * Math.pow(1.3,upgrade_mask-1));
 	if (cells >= upgrade_mask_cost) {
 		upgrade_mask = upgrade_mask + 1;
 		cells = cells - upgrade_mask_cost;
 		document.getElementById('upgrade_mask_amount').innerHTML = upgrade_mask;
 		document.getElementById('cells_amount').innerHTML = cells;
-	};
+	}
 	var nextCost = Math.floor(upgrade_cost[3] * Math.pow(1.3,upgrade_mask-1));
 	document.getElementById('upgrade_mask_cost').innerHTML = nextCost;
-};
+}
 
 
 //Minigames
 function guessNumber(){
 	var userGuess = document.getElementById('userGuess').value;
-	var correctNum = Math.floor(Math.random() * 11);
+	var correctNum = getRandomIntInclusive(0,10);
 	if(cells >= 10){
 		cells = Math.floor(cells * 0.9);
 		if (userGuess == correctNum){
 			cells = cells * 3;
-			document.getElementById('guessStatus').innerHTML = "Win! Cells doubled."
+			document.getElementById('guessStatus').innerHTML = "Win! Cells doubled.";
 		}
 		else{
-			document.getElementById('guessStatus').innerHTML = "Lost! 10% of cells gone."
+			document.getElementById('guessStatus').innerHTML = "Lost! 10% of cells gone.";
 		}
 	}
 	else{
-		document.getElementById('guessStatus').innerHTML = "You need 10 or more cells."
+		document.getElementById('guessStatus').innerHTML = "You need 10 or more cells.";
 	}
-};
+}
 
 function allOrNothingSlider(){
 	var cell_amount = document.getElementById("sliderAllOrNothing");
@@ -155,7 +156,7 @@ function allOrNothingSlider(){
 	cells_to_gamble_1 = cells_to_gamble;
 	cells_to_gamble_1 = Math.floor(cells_to_gamble_1 +(cells/10)*(cell_amount.value));
 	document.getElementById("allOrNothingValue").innerHTML = cells_to_gamble_1;
-};
+}
 
 function allOrNothing(){
 
@@ -177,13 +178,13 @@ function allOrNothing(){
 	else{
 		document.getElementById("allOrNothingStatus").innerHTML = "Not enough cells!";
 	}
-};
+}
 
 function getRandomIntInclusive(min, max) {
   var min = Math.ceil(min);
   var max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
-};
+}
 
 
 
@@ -202,31 +203,31 @@ function buildingMenu(evt, what){
 
 
 	for(var i = 0; i < tabs.length; i++){
-		tab = tabs[i]
+		tab = tabs[i];
 		document.getElementById(tab+"Container").style.visibility = 'hidden';
 		document.getElementById(what+"Container").style.visibility = 'visible';
 
 
 		}
 
-};
+}
 
 
 
 
 function cheat(){
 	cells = 500000;
-};
+}
 
 window.setInterval(function(){
 	infectClick(building_flies*building_multiplier[0]*upgrade_flies);
 	infectClick(building_hands*building_multiplier[1]*upgrade_hands);
 	infectClick(building_mask*building_multiplier[2]*upgrade_mask);
 	cellsPerSecond();
-	cpsUpdate()
+	cpsUpdate();
 
 }, 1000);
 
 window.setInterval(function(){
-	allOrNothingSlider()
+	allOrNothingSlider();
 }, 100);
