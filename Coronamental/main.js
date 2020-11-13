d627313b688166fc0fdb2f4bfd6df4e2ba92cf0b
Jeffery <up937100@myport.ac.uk>
2020-11-13 16:29:08 +0000

    Tabs added, items added and more
    DNA/s added
    tabs layout setup

--- Coronamental/main.js
+++ Coronamental/main.js
@@ -4,7 +4,8 @@ var player ={
         dna: 0.0,
         cell: 0.0
     },  //currency
-    clickPower: 1
+    clickPower: 1,
+    gameTick: 1000,
 
 };
 
@@ -16,6 +17,7 @@ var items = {
         cost: 10,
         level: 1,
         income: 1,
+        tier: 1,
     },
     hands: {
         id: "hands",
@@ -24,7 +26,17 @@ var items = {
         cost: 200,
         level: 1,
         income: 10,
-    }
+        tier: 2,
+    },
+    food: {
+        id: "food",
+        name: "Infected Food",
+        amount: 0,
+        cost: 750,
+        level: 1,
+        income: 25,
+        tier: 3,
+    },
 };
 
 function initialize() {
@@ -32,11 +44,30 @@ function initialize() {
 
 }
 
+function format(amount){
+    let power = Math.floor(Math.log10(amount));
+    let mantissa = amount / Math.pow(10,power);
+    if(power < 3) return amount.toFixed(0);
+    return mantissa.toFixed(2) + "e" + power;
+}
+
+
+function calcDnaPS(){
+    var totalPS = 0;
+    for(var item in items){
+        totalPS += (items[item].income * items[item].amount * items[item].level);
+    }
+    return totalPS;
+}
+
+function updateDnaPS(){
+    $("#dnaPS").html(calcDnaPS()+" DNA/s");
+}
+
 function addCurrency(val,id) {
     var currency;
     switch (id){
         case 'dna': player.currencies.dna += val; break;
-        
         case 'cell': 
         if(player.currencies.dna >= 100){player.currencies.dna -= 100;player.currencies.cell += val;}
         else(console.log("Error, not enough dna"));
@@ -52,7 +83,13 @@ function clickCurrency(id) {
 
 function updateCurrencyUI() {
         for (var val in player.currencies){
-            $("#" + val +"Amt").html(player.currencies[val]);
+            $("#" + val +"Amt").html(format(player.currencies[val]));
+        }
+        if (player.currencies.dna >= 100){
+            $("#cellClick").removeClass("disabled");
+        }
+        else{
+            $("#cellClick").addClass("disabled");
         }
 }
 
@@ -65,37 +102,57 @@ function updateItemsUI() {
             $("." + val + "Container").css({color: 'red'});
         }
         $("#" + val +"Name").html(items[val].name);
-        $("#" + val +"Amount").html("Amount: " + items[val].amount);
-        $("#" + val +"Cost").html("Cost: " + items[val].cost);
-        $("#" + val +"Level").html("Level: " + items[val].level);
+        $("#" + val +"Amount").html("Amount: " + format(items[val].amount));
+        $("#" + val +"Cost").html("Cost: " + format(items[val].cost));
+        $("#" + val +"Level").html("Level: " + format(items[val].level));
     }
 }
 
 
 function clickItem(val){
+    var currItem;
     for (var item in items){
-        $("." + item + "Container").click(buyItem(val));
+        currItem = item;
     }
+    $("." + currItem + "Container").click(buyItem(val));
+
 }
 
 function buyItem(val){
     if(player.currencies.dna >= items[val].cost){
         player.currencies.dna -= items[val].cost;
-        items[val].cost += (items[val].amount + 1) * 1.25;
+        items[val].cost = (items[val].cost * 1.1) + ((items[val].amount*0.25)+1);
         items[val].amount += 1;
+        console.log("cost" + items[val].cost);
+    }
+}
+
+function itemGain(){
+    for(var item in items){
+        player.currencies.dna += (items[item].income * items[item].amount * items[item].level);
     }
 }
 
 
 
-
-
-window.setInterval(function(){
+function updateAll(){
     updateCurrencyUI();
     updateItemsUI();
-},50);
+    updateDnaPS();
+}
 
-window.setInterval(function(){
-    updateCurrencyUI();
-    updateItemsUI();
-},500);
+function gameLoop(){
+    var gameTick = player.gameTick;
+
+    window.setInterval(function(){
+        itemGain();
+        calcDnaPS();
+    },gameTick);
+
+    window.setInterval(function(){
+        updateAll();
+    },50);
+    //Game updates UI every 50ms.
+}
+
+gameLoop();
