2150e6445bd2485091f54b038c1deda6bdfab46d
Jun Hui Chia <up937100@myport.ac.uk>
2020-12-14 19:19:18 +0000

    Finished off "Projects" section

--- Portfolio/main.js
+++ Portfolio/main.js
@@ -1,3 +1,22 @@
+;(function($, win) { // Taken from stackoverflow https://stackoverflow.com/questions/24768795/get-the-visible-height-of-a-div-with-jquery/26831113#26831113
+    $.fn.inViewport = function(cb) {
+       return this.each(function(i,el){
+         function visPx(){
+           var H = $(this).height(),
+               r = el.getBoundingClientRect(), t=r.top, b=r.bottom;
+           return cb.call(el, Math.max(0, t>0? H-t : (b<H?b:H)));  
+         } visPx();
+         $(win).on("resize scroll", visPx);
+       });
+    };
+  }(jQuery, window));
+
+$(".projects-content").inViewport(function(px){
+    if(px){
+        $(".project-box").addClass("animate");
+        }
+})
+
 $(document).ready(function(){
     $(window).scroll(function(){
         if(this.scrollY > 20){
